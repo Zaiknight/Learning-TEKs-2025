e6a93990e970a5567debb76f1eb916d308a1b0f8
Zaiknight <szainali234@gmail.com>
2025-07-10 16:10:25 +0500

    Day 16: Node.js, Express.js, Typescript and PostgreSQL

---
+++ JULY/notesApp/src/App.tsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+
+
+  return (
+    <main className="bg-gray-900 w-screen h-screen">
+      <h2 className="text-center text-amber-200 justify-center font-semibold text-4xl"></h2>
+    </main>
+  )
+}
+
+export default App
